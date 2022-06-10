254acfc3e278ef5210986767a63ecfec64e1d1bf
Leonardo Airam Muniz Cruz <aiiramcruz@gmail.com>
2022-06-10 10:47:39 -0300

    Criando classe Transaction; Instalando pacotes; Aplicando estilos.

--- projeto_despesas_pessoais/lib/main.dart
+++ projeto_despesas_pessoais/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:projeto_despesas_pessoais/model/Transaction.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(const DespesasApp());
 
@@ -18,12 +20,80 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final _transactions = [
+      Transaction(
+        id: 't1',
+        title: 'Novo Tênis de Corrida',
+        value: 310.76,
+        date: DateTime.now(),
+      ),
+      Transaction(
+        id: 't2',
+        title: 'Conta de Luz',
+        value: 211.30,
+        date: DateTime.now(),
+      ),
+    ];
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Despesas Pessoas"),
       ),
-      body: Center(
-        child: const Text("Versão Inicial"),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const SizedBox(
+            child: Card(
+              color: Colors.blue,
+              elevation: 5,
+              child: Text("Grafico"),
+            ),
+          ),
+          Column(
+            children: _transactions.map((e) {
+              return Card(
+                child: Row(children: [
+                  Container(
+                    margin: const EdgeInsets.symmetric(
+                      horizontal: 15,
+                      vertical: 10,
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.purple, width: 2),
+                    ),
+                    padding: const EdgeInsets.all(10),
+                    child: Text(
+                      'R\$ ${e.value.toStringAsFixed(2)}',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple,
+                      ),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        e.title,
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        DateFormat('d MMM y').format(e.date),
+                        style: const TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                ]),
+              );
+            }).toList(),
+          )
+        ],
       ),
     );
   }
